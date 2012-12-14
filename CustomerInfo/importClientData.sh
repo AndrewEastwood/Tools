b10701912f85f53fb8d4b240ad0155085ab469bf
Andrew Eastwood <ua.clients.support@gmail.com>
2012-12-14 09:57:02 -0600

    minor fix

--- CustomerInfo/importClientData.sh
+++ CustomerInfo/importClientData.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-echo `mongo $1 --quiet --eval 'db.clients.find({},{_id:0}).forEach(printjson)'` > data/allClients.secure
-echo 'All client data cards were imported'
+if [$1 === '']
+then
+    echo 'importClientData.sh <dbhost>';
+    exit 1;
+else
+    echo `mongo $1 --quiet --eval 'db.clients.find({},{_id:0}).forEach(printjson)'` > data/allClients.secure
+    echo 'All client data cards were imported'
+fi
 
 phantomjs lib/convertClientDataToJiraCSV.js 
