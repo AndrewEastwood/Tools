f2bc23ba73de0f39b1983c2d18ddee3a82cf1dda
Andrew Eastwood <ua.clients.support@gmail.com>
2012-12-17 09:48:34 -0600

    client info

--- CustomerInfo/importClientData.sh
+++ CustomerInfo/importClientData.sh
@@ -7,13 +7,13 @@ then
     echo 'importClientData.sh <dbhost>';
     exit 1;
 else
-    if [ -a CLIDATA ] && [ "$2" == "force" ]
+    if [ ! -e CLIDATA ] || [ "$2" == "force" ]
     then
         mongo $1 --quiet cimport.js > $CLIDATA;
-        echo "All client data cards were importe";
+        echo "All client data cards were imported";
     else
         echo "Using existed data file";
     fi
 fi
 
-phantomjs convertClientDataToJiraCSV.js $CLIDATA
+#phantomjs convertClientDataToJiraCSV.js $CLIDATA
