440c35ad6e093183e38957e681de373fc7756f6e
Andrew Eastwood <ua.clients.support@gmail.com>
2012-12-17 09:54:48 -0600

    client info

--- CustomerInfo/cimport.js
+++ CustomerInfo/cimport.js
@@ -17,9 +17,9 @@ while(cursor.hasNext() && itemsToPrint > 0){
 	//print(j.name);
     //var json = printjson();
 
-    clients.push(j);
+    clients.push(cursor.next());
 
-    if (cursor.hasNext())
+    //if (cursor.hasNext())
     //print(",");
     itemsToPrint--;
 }
@@ -27,123 +27,125 @@ while(cursor.hasNext() && itemsToPrint > 0){
 
 exportToCSV(getCSVFields(), getClientsInfo(clients));
 
-    function getClientsInfo(jsonDataArray) {
-    
-        //console.log(jsonDataArray);
+function getClientsInfo(jsonDataArray) {
 
-        var jiraClientsJson = {};
-        for (var key in jsonDataArray)
-            if (jsonDataArray[key].name != "" && jsonDataArray[key].uiVersion != "") {
-                jiraClientsJson[jsonDataArray[key].name] = new clientDataObject(jsonDataArray[key], jiraClientsJson);
+    //console.log(jsonDataArray);
+
+    ("");
+
+    var jiraClientsJson = {};
+    for (var key in jsonDataArray)
+        if (jsonDataArray[key].name != "" && jsonDataArray[key].uiVersion != "") {
+            jiraClientsJson[jsonDataArray[key].name] = new clientDataObject(jsonDataArray[key], jiraClientsJson);
+        }
+
+    return jiraClientsJson;
+}
+
+function clientDataObject(rawData, allRawData) {
+    this.getBundleName = function () { 
+        return rawData.bundle;
+    }
+    this.getDirectoryName = function () { 
+        return rawData.directory;
+    }
+    this.getName = function () { 
+        return rawData.name;
+    }
+    this.getIGLink = function (path, linkTitle) {
+        if (typeof(linkTitle) == "undefined" || linkTitle == "")
+            return "https://ig.bazaarvoice.com/xref/customers-trunk/" + path;
+        return "[" + linkTitle + "|" + this.getIGLink(path) + "]";
+    }
+    this.keyValueString = function (k, v) {
+        return " *" + k + "*: (" + v + ") ";
+    }
+    this.getJiraDescription = function (escape) {
+        // generate table style description for jira
+
+        var desc = {};
+
+        desc["UI Version"] = this.getUIversion(this.getDirectoryName());
+        desc.directory = this.getIGLink(this.getDirectoryName(), this.getDirectoryName());
+        desc["bundle folders"] = [];
+        for (var idx in rawData.bundleFolders)
+            desc["bundle folders"].push(this.getIGLink(rawData.bundleFolders[idx], rawData.bundleFolders[idx]));
+
+        desc["authentication"] = [];
+        for (var idx in rawData.authentication.map)
+            for (var dc in rawData.authentication.map[idx])
+                desc["authentication"].push(this.keyValueString(dc, rawData.authentication.map[idx][dc]));
+
+        desc["display codes"] = [];
+        for (var dcName in rawData.hosted.displayCodes)
+            desc["display codes"].push(this.getIGLink(this.getDirectoryName() + '/displays/' + dcName, dcName));
+
+        desc["locales"] = rawData.hosted.locales;
+        desc["other products"] = rawData.hosted.products;
+
+        for (var feedType in rawData.feeds) {
+            desc["feed " + feedType] = [];
+            for (var feedCat in rawData.feeds[feedType]) {
+                var category = [];
+                for (var feedIdx in rawData.feeds[feedType][feedCat])
+                    if (feedCat == "scheduled")
+                        category.push(rawData.feeds[feedType][feedCat][feedIdx]);
+                    else
+                        category.push(feedIdx + "=" + rawData.feeds[feedType][feedCat][feedIdx]);
+                desc["feed " + feedType].push(this.keyValueString(feedCat, category.join('; ')));
             }
+        }
 
-        return jiraClientsJson;
+        /*desc += "|display(s)|" + "";
+        desc += "|submission|" + "";
+        desc += "|display codes|" + "";
+
+        desc += "|esp|" + "";
+        desc += "|export feeds|" + "";
+        desc += "|product feed|" + "";
+        desc += "|overrides|" + "";
+        */
+
+        var stringDesc = "";
+        var value = "";
+        for (var p in desc) {
+            if (Array.isArray(desc[p]))
+                value = desc[p].join(' \\\\ ');
+            else
+                value = desc[p];
+            value = value || "undefined";
+            stringDesc += "|" + p + "|" + (escape?value.replace(/\,/g,"\\,").replace(/\"/g, '\\"'):value) + "| ";
+        }
+
+        return stringDesc;
     }
-    
-    function clientDataObject(rawData, allRawData) {
-        this.getBundleName = function () { 
-            return rawData.bundle;
-        }
-        this.getDirectoryName = function () { 
-            return rawData.directory;
-        }
-        this.getName = function () { 
-            return rawData.name;
-        }
-        this.getIGLink = function (path, linkTitle) {
-            if (typeof(linkTitle) == "undefined" || linkTitle == "")
-                return "https://ig.bazaarvoice.com/xref/customers-trunk/" + path;
-            return "[" + linkTitle + "|" + this.getIGLink(path) + "]";
-        }
-        this.keyValueString = function (k, v) {
-            return " *" + k + "*: (" + v + ") ";
-        }
-        this.getJiraDescription = function (escape) {
-            // generate table style description for jira
-
-            var desc = {};
-
-            desc["UI Version"] = this.getUIversion(this.getDirectoryName());
-            desc.directory = this.getIGLink(this.getDirectoryName(), this.getDirectoryName());
-            desc["bundle folders"] = [];
-            for (var idx in rawData.bundleFolders)
-                desc["bundle folders"].push(this.getIGLink(rawData.bundleFolders[idx], rawData.bundleFolders[idx]));
-
-            desc["authentication"] = [];
-            for (var idx in rawData.authentication.map)
-                for (var dc in rawData.authentication.map[idx])
-                    desc["authentication"].push(this.keyValueString(dc, rawData.authentication.map[idx][dc]));
-
-            desc["display codes"] = [];
-            for (var dcName in rawData.hosted.displayCodes)
-                desc["display codes"].push(this.getIGLink(this.getDirectoryName() + '/displays/' + dcName, dcName));
-
-            desc["locales"] = rawData.hosted.locales;
-            desc["other products"] = rawData.hosted.products;
-
-            for (var feedType in rawData.feeds) {
-                desc["feed " + feedType] = [];
-                for (var feedCat in rawData.feeds[feedType]) {
-                    var category = [];
-                    for (var feedIdx in rawData.feeds[feedType][feedCat])
-                        if (feedCat == "scheduled")
-                            category.push(rawData.feeds[feedType][feedCat][feedIdx]);
-                        else
-                            category.push(feedIdx + "=" + rawData.feeds[feedType][feedCat][feedIdx]);
-                    desc["feed " + feedType].push(this.keyValueString(feedCat, category.join('; ')));
-                }
-            }
-
-            /*desc += "|display(s)|" + "";
-            desc += "|submission|" + "";
-            desc += "|display codes|" + "";
-
-            desc += "|esp|" + "";
-            desc += "|export feeds|" + "";
-            desc += "|product feed|" + "";
-            desc += "|overrides|" + "";
-            */
-
-            var stringDesc = "";
-            var value = "";
-            for (var p in desc) {
-                if (Array.isArray(desc[p]))
-                    value = desc[p].join(' \\\\ ');
-                else
-                    value = desc[p];
-                value = value || "undefined";
-                stringDesc += "|" + p + "|" + (escape?value.replace(/\,/g,"\\,").replace(/\"/g, '\\"'):value) + "| ";
-            }
-
-            return stringDesc;
-        }
-        this.getPhase = function() {
-            return rawData.phase;
-        }
-        this.getUIversion = function (clientDirName) {
-            return this.getIGLink(clientDirName + "/config/bundleConfiguration.xml", rawData.uiVersion || "unable to identify");
-        }
-        this.toString = function () {
-            var str = "";
-            str += 'Story,';
-            str += '"Upgrade ' + this.getName() + '",';
-            str += '"' + this.getJiraDescription(true) + '",';
-            str += '"' + "Unscheduled - Phase " + this.getPhase() + '",';
-            str += '"' + this.getName() + '",';
-            str += '"' + this.getName() + '"';
-            return str;
-        }
+    this.getPhase = function() {
+        return rawData.phase;
     }
-    
-    function getCSVFields () {
-        return "Type,Summary,Description,Version,Client,Label";
+    this.getUIversion = function (clientDirName) {
+        return this.getIGLink(clientDirName + "/config/bundleConfiguration.xml", rawData.uiVersion || "unable to identify");
     }
-    function exportToCSV (fields, jsonData) {
-        print(fields);
-        var itemsToPrint = 10;
-        for (var key in jsonData) {
-            print(jsonData[key].toString());
-            if (itemsToPrint-- < 0)
-                break;
-        }
+    this.toString = function () {
+        var str = "";
+        str += 'Story,';
+        str += '"Upgrade ' + this.getName() + '",';
+        str += '"' + this.getJiraDescription(true) + '",';
+        str += '"' + "Unscheduled - Phase " + this.getPhase() + '",';
+        str += '"' + this.getName() + '",';
+        str += '"' + this.getName() + '"';
+        return str;
     }
+}
+
+function getCSVFields () {
+    return "Type,Summary,Description,Version,Client,Label";
+}
+function exportToCSV (fields, jsonData) {
+    print(fields);
+    var itemsToPrint = 10;
+    for (var key in jsonData) {
+        print(jsonData[key].toString());
+        if (itemsToPrint-- < 0)
+            break;
+    }
+}
