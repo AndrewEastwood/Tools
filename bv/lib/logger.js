9d22fd96a4c6340e61a4d3f524c3c79a329dedb4
AndrewEastwood <ua.clients.support@gmail.com>
2012-12-18 21:39:34 -0600

    logger changes

--- bv/lib/logger.js
+++ bv/lib/logger.js
@@ -1,54 +1,80 @@
-var _loggers = {
-	anonymous: {logdata : "", cache : true}
-};
-var _currentOwner = false;
+var glLoggers = {};
+var glUtil = require('util');
 
+function loggerClass (name) {
 
-function logger (name) {
+    // logger internal default settings
     var _cache = "";
-    var _name = name || "anonymous";
+    var _name = "anonymous";
+    var _cacheMode = false;
+    var _msgFmt = "[%s] %s %s %s %s";
+    var _fileNamePattern = "%s.log";
 
-    this.getName = function() { return this._name; }
-    this.log = function  () {}
-    this.save = function () {}
+    if (Array.isArray(name))
+        this.setup(name);
+    if (typeof(name) === 'string')
+        this._name = name;
+
+    this.setup = function (config) {
+        for (var p in config)
+            this['_' + p] = config[p];
+        return this;
+    }
+    this.getName = function () { return this._name; }
+    this.getID = function() { return this._name == "anonymous" ? "" : this._name.toUpperCase(); }
+    this.log = function  (text, level) {
+        var message = this.getMessage(text, level);
+        console.log(message);
+        // will be stored into the file
+        if (this.getCacheMode)
+            this._cache += message;
+        return this;
+    }
+    this.getMessage = function (text, level) {
+        var sign = " ";
+        level = level || "info";
+        // show short signs with messages
+        if (level === 'warn') sign = "!";
+        if (level === 'error') sign = "*";
+        if (level === 'fail') sign = "X";
+        return glUtil.format(this._msgFmt,
+            level.toUpperCase(),
+            this.getID(),
+            new Date().toLocaleTimeString(),
+            sign,
+            msg
+        );
+    }
+    this.getCacheMode = function (mode) { return this._saveMessages; }
+    this.setCacheMode = function (mode) { this._saveMessages = mode; }
+    this.getFileName = function () {
+        return glUtil.format(this._fileNamePattern, this.getName);
+    }
+    this.close = function () {
+        if (!this.getCacheMode())
+            return false;
+        var fs = require('fs');
+        fs.writeFile(this.getFileName(), this._cache);
+        return true;
+    }
 }
 
-exports.setup = function (owner, useCaching) {
-	_currentOwner = this.getCurrentOwner(owner);
-	if (typeof(_loggers[_currentOwner]) == "undefined")
-		_loggers[_currentOwner] = {logdata : "", cache : true};
-	else
-		_loggers[_currentOwner].cache = useCaching;
-    return exports;
+
+/******** module implementation ********/
+
+exports.log = function (msg, level, loggerName) {
+    console.log(__filename);
+    //if (typeof(lo))
 }
 
-exports.getCurrentOwner = function (owner) { return owner || this._currentOwner || "anonymous"; }
-
-exports.export = function (owner) {
-	if (_loggers[owner || _currentOwner] && _loggers[owner || _currentOwner].cache)
-		return _loggers[owner || _currentOwner].logdata;
-	return "";
-}
-
-exports.getAllLoggers()
-
-exports.save = function (owner) {
-    //var log = _loggers[this.getCurrentOwner(owner)]
-}
-
-exports.log = function (msg, level, owner) {
-	//console.log(msg);
-    var sign = " ";
-    level = level || "info";
-    // show short signs with messages
-    if (level === 'warn') sign = "!";
-    if (level === 'error') sign = "*";
-    if (level === 'fail') sign = "X";
-    // log message
-    var message = "[" + level.toUpperCase() + "] " + (()) + new Date().toLocaleTimeString() + " " + sign + " : " + msg;
-    console.log(message);
-    // will be stored into the file
-    //this.cache += "\n" + message;
-    if (typeof(_loggers[owner || _currentOwner]) !== "undefined" && _loggers[owner || _currentOwner].cache)
-    	_loggers[owner || _currentOwner].logdata += message;
+exports.geLogger = function (loggerName) {
+    var logger = false;
+    loggerName = loggerName.toUpperCase();
+    if (glLoggers.hasOwnProperty(loggerName))
+        logger = glLoggers[loggerName];
+    else {
+        logger = new loggerClass(loggerName);
+        glLoggers[loggerName] = logger;
+    }
+    return logger;
 }
