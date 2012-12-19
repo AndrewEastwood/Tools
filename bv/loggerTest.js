2ec92c30259cf3a11b61bc8530621c909e32254e
aivaskev <aivaskev@aivaskev-desktop.(none)>
2012-12-19 00:54:51 -0600

    logger is updated

--- bv/loggerTest.js
+++ bv/loggerTest.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var logger1 = require('./lib/logger.js').getLogger("demo1");
 var logger2 = require('./lib/logger.js').getLogger("demo2");
 var loggerAn = require('./lib/logger.js');
@@ -8,6 +7,3 @@ var loggerAn = require('./lib/logger.js');
 logger1.log('xxxxxx');
 logger2.log('yyyyyyyy');
 loggerAn.log('zzzzzzzzz');
-
-=======
->>>>>>> a8d8e2f5f49fbfd460969bc7d9a8e6068ee0d6d2
