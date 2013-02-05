2ba16a0b4e18a56687e4299f08119a9f6e9a7c6f
Andriy <andriy@Leeds.(none)>
2013-02-05 17:12:32 -0600

    updated tools

--- bv/tools/loggerTest.js
+++
@@ -1,9 +0,0 @@
-var logger1 = require('../lib/logger.js').getLogger("demo1");
-var logger2 = require('../lib/logger.js').getLogger("demo2");
-var loggerAn = require('../lib/logger.js');
-
-
-
-logger1.log('xxxxxx');
-logger2.log('yyyyyyyy');
-loggerAn.log('zzzzzzzzz');
